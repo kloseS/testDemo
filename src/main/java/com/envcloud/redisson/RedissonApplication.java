ea279bd7693a31dfc891974021450dbe20aabde1
kloseS <13913192896@163.com>
2020-04-20 15:09:42 +0800

    123

--- src/main/java/com/envcloud/redisson/RedissonApplication.java
+++ src/main/java/com/envcloud/redisson/RedissonApplication.java
@@ -7,7 +7,10 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan(value = "com.envcloud.redisson.repository")
 public class RedissonApplication {
-
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(RedissonApplication.class, args);
     }
